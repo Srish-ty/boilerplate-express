52b5e7e992b1f890c2dfb8ae1fbd5f648880d10c
Srishty Mangutte <srishtymangutte@gmail.com>
2023-01-10 16:47:30 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,7 +2,22 @@ let express = require('express');
 let app = express();
 
 
+console.log("Hello World")
 
+//Serve an Html file
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+})
+
+//serve static assests
+abspath = __dirname +"/public"
+app.use("/public", express.static(abspath))
+
+
+//serve json
+app.get("/json", function(req,res){
+  res.json({"message": "Hello json"});
+})
 
 
 
